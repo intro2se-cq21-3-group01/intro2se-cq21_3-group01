766bc671f346e599fd60f21a989560e141bc515d
kuwelym <klyminh@gmail.com>
2024-01-20 01:18:54 +0700

    Add email and password validation in Register component

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -13,6 +13,17 @@ const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const isEmailValid = (email) => {
+        const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+        return emailRegex.test(email);
+    };
+
+    const isPasswordValid = (password) => {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+        return passwordRegex.test(password);
+    };
+
+
     const navigate = useNavigate();
 
     const handleRegister = async (e) => {
@@ -20,6 +31,16 @@ const Register = () => {
 
         if (!username || !email || !password) {
             toast.error("Please enter email, username and password !");
+            return;
+        }
+
+        if (!isEmailValid(email)) {
+            toast.error("Please enter a valid email address!");
+            return;
+        }
+
+        if (!isPasswordValid(password)) {
+            toast.error("Password must have at least 1 uppercase letter, 1 number, and be 8 characters long!");
             return;
         }
 
